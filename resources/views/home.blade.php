7e3ffbf0a535fc38d45627293d3b4fe74d887b56
Therese September <the-missen@hotmail.com>
2018-03-24 18:21:20 +0100

    .hot
    .lækker

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<section class="hero is-primary">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Budgets
+      </h1>
+      <h2 class="subtitle">
+        - Welcome back!
+      </h2>
     </div>
+  </div>
+</section>
+<section class="section">
+<div class="container">
+  
+    <a class="button is-primary" style="float:left;">
+      <span class="icon">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>New Budget</span> 
+    </a>
+  <nav class="breadcrumb is-right" aria-label="breadcrumbs">
+    <ul>
+      <li><a href="#">Bulma</a></li>
+      <li><a href="#">Documentation</a></li>
+      <li><a href="#">Components</a></li>
+      <li class="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
+    </ul>
+  </nav>
+  <div class="columns is-multiline">
+    @include('partials.info')
+    @include('partials.info')
+    @include('partials.info')
+    @include('partials.info')
+  </div>
 </div>
+</section>
 @endsection
